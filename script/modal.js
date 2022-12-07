41905b02564d9c5f8bce62b2a27de2725da469f8
Maks <mkorablov30@gmail.com>
2022-12-07 22:19:24 +0200

    desktop and modal window

---
+++ script/modal.js
@@ -0,0 +1,11 @@
+$('.modal__overlay').on('click', function(e){
+    if($(e.target).closest('.modal').length == 0){
+        $(this).fadeOut();
+    }
+});
+$('.close').on('click', function(){
+    $(this).parents('.modal__overlay').fadeOut();
+});
+$('#open__modal').on('click', function(){
+    $('.modal__overlay').fadeIn();
+})
